38079d9ee40f2bd48f3cf3a242f226ace8c21768
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-11-08 15:21:09 +0700

    Custom404

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+    return (
+        <div style={{ color: 'darkgray',textAlign: 'center' }}>
+            Oppps <br />
+            Halaman yang anda cari tidak ditemukan
+        </div>
+    )
+}
