47378b305f0ecec3803c549f5f325254ba607d36
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-11-10 22:08:58 +0700

    add auto redirection using useRouter

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,18 @@
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
+
 export default function Custom404() {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+        
+    }, [])
+
     return (
         <div style={{ color: 'darkgray',textAlign: 'center' }}>
             Oppps <br />
